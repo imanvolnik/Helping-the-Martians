da99837c65d1a5066b639ad314b4cfeb8a46d9b8
Iman <mashrapov2005@gmail.com>
2023-11-29 13:59:18 +0600

    Fourth commit (modified functions)

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import os
+import random
 
 total_weight = 713
 
-def get_kilo_marks():
+def create_boxmarks():
     while True:
-        point_A = int(input("Box 1 kilometer (from 1 to 7): "))
-        point_B = int(input("Box 2 kilometer (from 1 to 7): "))
-        point_C = int(input("Box 3 kilometer (from 1 to 7): "))
-
-        if 1 <= point_A <= 7 and 1 <= point_B <= 7 and 1 <= point_C <= 7:
+        boxmark1 = random.randint(1, 7) 
+        boxmark2 = random.randint(1, 7) 
+        boxmark3 = random.randint(1, 7)
+        if boxmark1 != boxmark2 and boxmark2 != boxmark3:
             break
-        else:
-            print("Enter numbers from 1 to 7")
-
-    return point_A, point_B, point_C
+    return [boxmark1, boxmark2, boxmark3]
+    
 
 def get_box_weight():
     while True:
@@ -26,21 +24,11 @@ def get_box_weight():
         else:
             print("The total weight must be 713 kilograms")
 
-    return box1_weight, box2_weight, box3_weight
+    return [box1_weight, box2_weight, box3_weight]
 
-point_A, point_B, point_C = get_kilo_marks()
-box1_weight, box2_weight, box3_weight = get_box_weight()            
+lst_boxmarks = create_boxmarks()
+print(f"The kilometer box marks are {lst_boxmarks}")
+
+lst_boxweight = get_box_weight()            
 
 os.system("cls")
-
-while True:
-    check_A = int(input("Kilometer mark Box 1: "))
-    check_B = int(input("Kilometer mark Box 2: "))
-    check_C = int(input("Kilometer mark Box 3: "))
-
-    if check_A == point_A and check_B == point_B and check_C == point_C:
-        print("All kilometer marks are correct")
-        break
-    else:
-        point_A, point_B, point_C = get_kilo_marks()
-
