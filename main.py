e85eeeb2fe526a8ac1267c3a36d45a395b5bb23f
Iman <mashrapov2005@gmail.com>
2023-11-29 16:43:01 +0600

    Fifth commit

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ total_weight = 713
 
 def create_boxmarks():
     while True:
-        boxmark1 = random.randint(1, 7) 
-        boxmark2 = random.randint(1, 7) 
-        boxmark3 = random.randint(1, 7)
+        boxmark1 = random.randint(1, 3) 
+        boxmark2 = random.randint(3, 5) 
+        boxmark3 = random.randint(5, 7)
         if boxmark1 != boxmark2 and boxmark2 != boxmark3:
             break
     return [boxmark1, boxmark2, boxmark3]
@@ -26,9 +26,23 @@ def get_box_weight():
 
     return [box1_weight, box2_weight, box3_weight]
 
+def check_cargo():
+    while True:
+        print("Enter the kilometer marks to check for cargo:")
+        kilometer1 = int(input("Kilometer mark 1: "))
+        kilometer2 = int(input("Kilometer mark 2: "))
+        kilometer3 = int(input("Kilometer mark 3: "))
+
+        if kilometer1 == lst_boxmarks[0] and kilometer2 == lst_boxmarks[1] and kilometer3 == lst_boxmarks[2]:
+            print("Congratulations! You found all the cargo.")
+            break
+        else:
+            print("No cargo found at the entered kilometer marks. Boxes have moved.")
+
 lst_boxmarks = create_boxmarks()
-print(f"The kilometer box marks are {lst_boxmarks}")
+print(lst_boxmarks)
 
 lst_boxweight = get_box_weight()            
 
 os.system("cls")
+
