514c5b0ae703d545107497cc3dae636fac18245d
Iman <mashrapov2005@gmail.com>
2023-11-29 19:28:35 +0600

    Seventh commit

--- main.py
+++ main.py
@@ -1,50 +1,34 @@
-import os
 import random
+import os
 
 total_weight = 713
 
-boxmark1 = random.randint(1, 3) 
-boxmark2 = random.randint(3, 5) 
-boxmark3 = random.randint(5, 7)
-
 def move_boxes():
-    boxmark1 = random.randint(1, 3) 
-    boxmark2 = random.randint(3, 5) 
-    boxmark3 = random.randint(5, 7)
-    return boxmark1, boxmark2, boxmark3
+    return random.sample(range(1, 8), 3)
 
-def get_box_weight():
+def get_box_weights():
+    weights = []
+    remaining_weight = total_weight
+    for i in range(2):
+        weight = int(input(f"Enter weight of box {i+1} (remaining weight {remaining_weight}): "))
+        weights.append(weight)
+        remaining_weight -= weight
+    weights.append(remaining_weight)
+    return weights
+
+def check_cargo(box_locations, box_weights):
     while True:
-        box1_weight = int(input(f"Box 1 weight in kilograms ({total_weight} kilograms left): "))
-        box2_weight = int(input(f"Box 2 weight in kilograms ({total_weight - box1_weight} kilograms left): "))
-        box3_weight = int(input(f"Box 3 weight in kilograms ({total_weight - box1_weight - box2_weight} kilograms left): "))
-
-        if (box1_weight + box2_weight + box3_weight) == total_weight:
-            break 
-        else:
-            print("The total weight must be 713 kilograms")
-
-    return [box1_weight, box2_weight, box3_weight]
-
-def check_cargo():
-    while True:
-        print("Enter the kilometer marks to check for cargo:")
-        kilometer1 = int(input("Kilometer mark 1: "))
-        kilometer2 = int(input("Kilometer mark 2: "))
-        kilometer3 = int(input("Kilometer mark 3: "))
-
-        if kilometer1 == boxmark1 and kilometer2 == boxmark2 and kilometer3 == boxmark3:
+        guessed_locations = list(map(int, input("Enter guessed locations for all boxes, separated by spaces: ").split()))
+        if sorted(guessed_locations) == sorted(box_locations) and sum(box_weights) == total_weight:
+            os.system('cls')
             print("Congratulations! You found all the cargo.")
             break
         else:
-            print("No cargo found at the entered kilometer marks. Boxes have moved.")
-            lst_boxmarks = [move_boxes()]
+            print("No cargo found at the guessed locations. The boxes have moved.")
+            box_locations = move_boxes()    
 
+box_locations = move_boxes()
 
-lst_boxmarks = [boxmark1, boxmark2, boxmark3]
+box_weights = get_box_weights()
 
-lst_boxweight = get_box_weight()            
-
-os.system("cls")
-
-check_cargo()
+check_cargo(box_locations, box_weights)
